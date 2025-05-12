d784c73f1e10f067234b91021c75f238338bbd3a
muhammadjamalmatloob <jamalmatloob786@gmail.com>
2025-05-12 21:12:38 +0500

    First Commit

---
+++ OOP_Game/Game/GameView.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Game
+{
+    partial class GameView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameView));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Update);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(71, 297);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GameView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "GameView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GameView";
+            this.Load += new System.EventHandler(this.Start);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.IsKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ OOP_Game/Game/GameView.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class GameView : Form
+    {
+        int JumpSpeed = 25;
+        int speed = 10;
+        int gravity = 2;
+        bool jump = false;
+        int Yinit;
+        bool move = false;
+        public GameView()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            JumpManager();
+        }
+
+        private void StartJump()
+        {
+            jump = true;
+            Yinit = pictureBox1.Location.Y;
+        }
+
+        private void JumpManager()
+        {
+            if (jump)
+            {
+                if (pictureBox1.Location.Y - gravity >= Yinit)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, Yinit);
+                    jump = false;
+                    JumpSpeed = 25;
+                }
+                else
+                {
+                    pictureBox1.Top -= JumpSpeed;
+                    JumpSpeed -= gravity;
+                }
+            }
+            
+
+        }
+
+        private void IsKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !jump)
+            {
+                StartJump();
+            }
+        }
+
+        private void MoveRight()
+        {
+
+        }
+    }
+}
